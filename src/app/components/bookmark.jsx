da3c88956fe3c8bde21dba2c039f7298540a9158
IURII <pikuliurii@gmail.com>
2022-07-19 13:56:18 -0300

    add pagination

--- src/app/components/bookmark.jsx
+++ src/app/components/bookmark.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BookMark = ({status, ...rest}) => {
     return (
     <button className="btn" {...rest}>
-        <i class={status? 'bi bi-person-check-fill' : 'bi bi-person'}></i>
+        <i className={status? 'bi bi-person-check-fill' : 'bi bi-person'}></i>
     </button>)
 }
 
--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import Pagination from "./pagination";
 import User from "./user";
+import { paginate } from "../utils/paginate";
 
 const Users = ({users, ...rest}) => {
+  const count = users.length
+  const pageSize = 4
+  const [currentPage, setCurrentPage] = useState(1); 
+  const handlePageChange = (pageIndex) => {
+    setCurrentPage(pageIndex);
+  }
+
+  const userCrop = paginate(users, currentPage, pageSize);
+
   return (
     <>
-      {users.length > 0 && (<table className="table">
+      {count > 0 && (<table className="table">
         <thead>
           <tr>
             <th scope="col">Имя</th>
@@ -16,7 +27,7 @@ const Users = ({users, ...rest}) => {
             <th scope="col"></th>
           </tr>
         </thead>
-        <tbody>{users.map((user) => (
+        <tbody>{userCrop.map((user) => (
               <User
                 key={user._id}
                 {...user}
@@ -24,6 +35,7 @@ const Users = ({users, ...rest}) => {
               />
             ))}</tbody>
       </table>)}
+      <Pagination itemsCount={count} pageSize={pageSize} onPageChange={handlePageChange} currentPage={currentPage}/>
     </>
   );
 };
